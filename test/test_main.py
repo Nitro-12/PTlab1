a885baa7b75981de5282835e37ce4dd6a37ade95
Nitr0-12 <ka4urin.nik@yandex.ru>
2024-09-15 00:51:23 +0300

    Fix bugs

--- test/test_main.py
+++ test/test_main.py
@@ -10,13 +10,16 @@ sys.path.insert(0, os.path.abspath(
 
 from main import get_path_from_arguments
 
+
 @pytest.fixture()
 def correct_arguments_string() -> tuple[list[str], str]:
     return ["-p", "/home/user/file.txt"], "/home/user/file.txt"
 
+
 @pytest.fixture()
 def noncorrect_arguments_string() -> list[str]:
     return ["/home/user/file.txt"]
+
 
 def test_get_path_from_correct_arguments(
     correct_arguments_string: tuple[list[str], str]
@@ -24,6 +27,7 @@ def test_get_path_from_correct_arguments(
     # Проверяем корректный путь
     path = get_path_from_arguments(correct_arguments_string[0])
     assert path == correct_arguments_string[1]
+
 
 def test_get_path_from_noncorrect_arguments(
     noncorrect_arguments_string: list[str]
