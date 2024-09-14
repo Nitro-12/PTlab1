45c9038e442281b5dc3caf98d314f74bd2873eb1
Nitro-12 <ka4urin.nik@yandex.ru>
2024-09-14 22:07:31 +0300

    Исправленные ошибки стандарт PEP

--- test/CalcRating.py
+++ test/CalcRating.py
@@ -5,17 +5,19 @@ import pytest
 
 RatingsType = dict[str, float]
 
+
 class TestCalcRating:
+
     @pytest.fixture()
     def input_data(self) -> tuple[DataType, RatingsType]:
         data: DataType = {
             "Абрамов Петр Сергеевич": [
-                ("математика", 80), 
+                ("математика", 80),
                 ("русский язык", 76),
                 ("программирование", 100)
             ],
             "Петров Игорь Владимирович": [
-                ("математика", 61), 
+                ("математика", 61),
                 ("русский язык", 80),
                 ("программирование", 78),
                 ("литература", 97)
