22d47b15b0dbb6441d62f01eb470519ee49c8823
Nitro-12 <ka4urin.nik@yandex.ru>
2024-09-15 00:21:27 +0300

    Fix errors

--- test/test_TextDataReader.py
+++ test/test_TextDataReader.py
@@ -5,13 +5,14 @@ import pytest
 
 
 # Путь к папке src в sys.path
-sys.path.insert(0, os.path.abspath(os.path.join
-                                   (os.path.dirname(__file__), '../src')))
+sys.path.insert(0, os.path.abspath
+		(os.path.join(os.path.dirname(__file__), '../src')))
 
 
 # Импорт модуля из папки src
 from Types import DataType
 from TextDataReader import TextDataReader
+
 
 class TestTextDataReader:
 
