d7b353f65ce6bc602d88379ed9cbbadda4e25a0b
Nitr0-12 <ka4urin.nik@yandex.ru>
2024-09-15 01:02:16 +0300

    Fix errors

--- test/test_TextDataReader.py
+++ test/test_TextDataReader.py
@@ -3,17 +3,13 @@ import os
 import sys
 import pytest
 
-
 # Путь к папке src в sys.path
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../src')
 ))
 
-
-
 from Types import DataType
 from TextDataReader import TextDataReader
-
 
 
 class TestTextDataReader:
--- test/test_main.py
+++ test/test_main.py
@@ -3,7 +3,6 @@ import os
 import sys
 import pytest
 
-
 # Добавляем путь к папке src в sys.path
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../src')
