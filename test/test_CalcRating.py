10340b0fd250553b7a5718923a18445111714973
Nitr0-12 <ka4urin.nik@yandex.ru>
2024-09-15 01:13:26 +0300

    Fix

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -3,13 +3,15 @@ import os
 import sys
 import pytest
 
+from Types import DataType
+from CalcRating import CalcRating
+
+
 # Путь к папке src в sys.path
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../src')
 ))
 
-from Types import DataType
-from CalcRating import CalcRating
 
 
 RatingsType = dict[str, float]
