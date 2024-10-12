efb6636a08c15a08dfed87c6d005ceae5555f8b5
Nitr0-12 <ka4urin.nik@yandex.ru>
2024-10-12 06:03:33 +0300

    fixed bags

--- src/Students_calc_var6.py
+++ src/Students_calc_var6.py
@@ -4,10 +4,9 @@ class StudentAnalyzer:
 
     def find_student(self):
         for student, grades in self.students.items():
-            
             count_grades = sum(1 for score in grades.values() if score == 76)
 
             if count_grades >= 3:
-                return student  
+                return student
 
         return "Студентов с минимум тремя оценками = 76 нет."
